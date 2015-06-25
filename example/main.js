8fdd7c0fb628bbed2cb062cf248e69d936f70c7d
Ionică Bizău <bizauionica@gmail.com>
2015-06-25 16:47:45 +0300

    Updated worky api

--- example/main.js
+++ example/main.js
@@ -2,8 +2,12 @@
     var w = new Worky("worker.js");
 
     // Listen for a specific event
-    w.on("some event", function (ev, data) {
+    w.on("some event", function (data) {
         console.log("Got some event and its data", data);
+    });
+
+    w.on("hello world", function (data) {
+        console.log(data);
     });
 
     console.log("Sending data to the worker via 'another event'");
