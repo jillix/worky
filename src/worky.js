215b5c85b159a618000226a2064bb965253945cf
Ionică Bizău <bizauionica@gmail.com>
2015-06-25 16:48:35 +0300

    Inherit Worky from EventEmitter

--- src/worky.js
+++ src/worky.js
@@ -1,57 +1,75 @@
 (function (root) {
 
-    var workers = {};
-
-    function Worky(script) {
-
-        if (this.constructor !== Worky && script) {
-            return new Worky(script);
-        }
-
-        var listeners = this.listeners = {};
-        this.is_worker = !script;
-
-        // Initialize the worker interface
-        if (script) {
-            this.worker = new Worker(script);
-            this.worker.addEventListener("message", function (ev) {
-                ev.data = Object(ev.data);
-                var l = listeners[ev.data.event];
-                if (!l || !l.length) { return; }
-                l.forEach(function (fn) {
-                    fn(ev, ev.data.data);
-                });
-            });
-        } else {
-            this.worker.postmessage(new Worky.Message("_init"));
-        }
+    function EventListener() {
+        this._ = {};
     }
 
-
-    Worky.Message = function (ev, data) {
-        this.event = ev;
-        this.data = data;
-    };
-
-    Worky.prototype.on = function (ev, fn) {
-        var l = this.listeners[ev] = this.listeners[ev] || [];
+    EventListener.prototype.on = function (ev, fn) {
+        var l = this._[ev] = this._[ev] || [];
         l.push(fn);
         return this;
     };
 
-    Worky.prototype.emit = function (ev, data) {
-        root.onmessage = function () {
+    EventListener.prototype.emit = function (_ev, ev, data) {
+        var self = this
+          , l = this._[ev]
+          , args = Array.prototype.slice.call(arguments, 2)
+          ;
 
-        };
-        var l = this.listeners[ev];
         if (!l || !l.length) {
             return this;
         }
 
         l.forEach(function (fn) {
-
+            fn.apply(_ev, args);
         });
     };
+
+    var _workers = {};
+
+    function Worky(script) {
+
+        var self = {};
+
+        if (this.constructor !== Worky && script) {
+            return new Worky(script);
+        }
+
+        if (!script) {
+            self = Object.create(EventListener.prototype);
+        } else {
+            self = this;
+        }
+
+        EventListener.call(self);
+        self.is_worker = !script;
+
+        // Initialize the worker interface
+        if (script) {
+            self.worker = new Worker(script);
+            self.worker.worky = this;
+            self.worker.onmessage = function (ev) {
+                ev.data = Object(ev.data);
+                self.emit(ev, ev.data.event, ev.data);
+            };
+            self.on("__init", function (ev, data) {
+                debugger
+            });
+        } else {
+            self.emit = function (ev, data) {
+                root.postMessage(new Worky.Message(ev, data));
+            };
+            return self;
+        }
+    }
+
+    Worky.prototype = Object.create(EventListener.prototype);
+    Worky.prototype.constructor = Worky;
+    Worky.Message = function (ev, data) {
+        this.event = ev;
+        this.data = data;
+    };
+
 
     root.Worky = Worky;
 })(self);
