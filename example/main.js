fc8e8ff4e35f13f24ab41fa81dea61b8481dcbee
Ionică Bizău <bizauionica@gmail.com>
2015-06-26 10:36:44 +0300

    Debugging why emitting does not work

--- example/main.js
+++ example/main.js
@@ -1,18 +1,19 @@
 (function () {
-    var w = new Worky("worker.js");
+    var w = new Worky("worker.js", function () {
 
+    });
     // Listen for a specific event
     w.on("some event", function (data) {
         console.log("Got some event and its data", data);
+        console.log("Sending data to the worker via 'another event'");
+        // Listen for a specific event
+        debugger
+        w.emit("another event", {
+            "hello": "world"
+        });
     });
 
     w.on("hello world", function (data) {
         console.log(data);
-    });
-
-    console.log("Sending data to the worker via 'another event'");
-    // Listen for a specific event
-    w.emit("another event", {
-        "hello": "world"
     });
 })();
--- example/worker.js
+++ example/worker.js
@@ -6,6 +6,7 @@ var worker = Worky();
 worker.emit("hello world", { "yes!": "it works!" });
 
 worker.on("another event", function (ev, data) {
+    debugger
     worker.write("some event", data);
     worker.write("foo", data);
 });
